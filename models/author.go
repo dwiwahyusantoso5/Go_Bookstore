8362c7d1e07f96ade83efd328ac97b5b95c6795c
Dwi Wahyu Santoso <dwi.wahyu.toso@gmail.com>
2022-11-01 22:12:38 +0700

    add author model

--- models/author.go
+++ models/author.go
@@ -1,1 +1,9 @@
 package models
+
+import "gorm.io/gorm"
+
+type Author struct {
+	gorm.Model
+	Firstname string `gorm:"not null" json:"firstname"`
+	Lastname  string `gorm:"not null" json:"lastname"`
+}
